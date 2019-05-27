ec0af803ea1baaa9630796be5c646301803df8a4
idkhromov <i.d.khromov@gmail.com>
2019-05-28 02:03:23 +0300

    Новое ПО для тестирования библиотеки PortsLib.dll, компонент библиотеки PortsLib.dll, подключеная библиотека CRC

---
+++ PortManager/FormMain.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PortManager
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.rtbCmd = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSendCmd = new System.Windows.Forms.Button();
+            this.ctlConnectionSettings1 = new PMControls.ctlConnectionSettings();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ctlCrcCalc1 = new CtlCrcCalc.CtlCrcCalc();
+            this.btnAppendCRC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rtbLog.Location = new System.Drawing.Point(320, 32);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtbLog.Size = new System.Drawing.Size(518, 240);
+            this.rtbLog.TabIndex = 2;
+            this.rtbLog.Text = "";
+            this.rtbLog.DoubleClick += new System.EventHandler(this.rtbLog_DoubleClick);
+            // 
+            // rtbCmd
+            // 
+            this.rtbCmd.Font = new System.Drawing.Font("Arial", 10.15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rtbCmd.Location = new System.Drawing.Point(320, 306);
+            this.rtbCmd.Name = "rtbCmd";
+            this.rtbCmd.Size = new System.Drawing.Size(518, 66);
+            this.rtbCmd.TabIndex = 3;
+            this.rtbCmd.Text = "0x01 0x02 0x03";
+            this.rtbCmd.TextChanged += new System.EventHandler(this.rtbCmd_TextChanged);
+            this.rtbCmd.DoubleClick += new System.EventHandler(this.rtbCmd_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Команда:";
+            // 
+            // btnSendCmd
+            // 
+            this.btnSendCmd.Location = new System.Drawing.Point(716, 378);
+            this.btnSendCmd.Name = "btnSendCmd";
+            this.btnSendCmd.Size = new System.Drawing.Size(122, 37);
+            this.btnSendCmd.TabIndex = 5;
+            this.btnSendCmd.Text = "Отправить";
+            this.btnSendCmd.UseVisualStyleBackColor = true;
+            this.btnSendCmd.Click += new System.EventHandler(this.btnSendCmd_Click);
+            // 
+            // ctlConnectionSettings1
+            // 
+            this.ctlConnectionSettings1.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.ctlConnectionSettings1.COM = "COM1";
+            this.ctlConnectionSettings1.GSM = false;
+            this.ctlConnectionSettings1.IP = "192.168.0.1";
+            this.ctlConnectionSettings1.IsTCPSelected = true;
+            this.ctlConnectionSettings1.Location = new System.Drawing.Point(12, 12);
+            this.ctlConnectionSettings1.Name = "ctlConnectionSettings1";
+            this.ctlConnectionSettings1.Size = new System.Drawing.Size(292, 377);
+            this.ctlConnectionSettings1.TabIndex = 0;
+            this.ctlConnectionSettings1.TCPPort = "1001";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Лог:";
+            // 
+            // ctlCrcCalc1
+            // 
+            this.ctlCrcCalc1.InputByteString = "";
+            this.ctlCrcCalc1.Location = new System.Drawing.Point(848, 12);
+            this.ctlCrcCalc1.Name = "ctlCrcCalc1";
+            this.ctlCrcCalc1.Size = new System.Drawing.Size(243, 389);
+            this.ctlCrcCalc1.TabIndex = 7;
+            // 
+            // btnAppendCRC
+            // 
+            this.btnAppendCRC.Location = new System.Drawing.Point(320, 378);
+            this.btnAppendCRC.Name = "btnAppendCRC";
+            this.btnAppendCRC.Size = new System.Drawing.Size(122, 37);
+            this.btnAppendCRC.TabIndex = 8;
+            this.btnAppendCRC.Text = "Дополнить CRC";
+            this.btnAppendCRC.UseVisualStyleBackColor = true;
+            this.btnAppendCRC.Click += new System.EventHandler(this.btnAppendCRC_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 433);
+            this.Controls.Add(this.btnAppendCRC);
+            this.Controls.Add(this.ctlCrcCalc1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSendCmd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbCmd);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.ctlConnectionSettings1);
+            this.Name = "FormMain";
+            this.Text = "Тестирование LibPorts v.1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PMControls.ctlConnectionSettings ctlConnectionSettings1;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.RichTextBox rtbCmd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSendCmd;
+        private System.Windows.Forms.Label label2;
+        private CtlCrcCalc.CtlCrcCalc ctlCrcCalc1;
+        private System.Windows.Forms.Button btnAppendCRC;
+    }
+}
+
---
+++ PortManager/FormMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using PollingLibraries.LibPorts;
+
+namespace PortManager
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAppendCRC_Click(object sender, EventArgs e)
+        {
+            string tmp = "";
+            ctlCrcCalc1.CalcCRCAndAppend(out tmp);
+            rtbCmd.Text = tmp;
+        }
+
+        private void rtbCmd_TextChanged(object sender, EventArgs e)
+        {
+            ctlCrcCalc1.InputByteString = rtbCmd.Text;
+        }
+
+        private void rtbCmd_DoubleClick(object sender, EventArgs e)
+        {
+            rtbCmd.Clear();
+        }
+
+        private void rtbLog_DoubleClick(object sender, EventArgs e)
+        {
+            rtbLog.Clear();
+        }
+
+        private void btnSendCmd_Click(object sender, EventArgs e)
+        {
+            VirtualPort vp = ctlConnectionSettings1.VirtualPort;
+
+            if (vp == null)
+            {
+                rtbLog.Text += "Виртуальный порт не создан\n";
+                return;
+            }
+
+            byte[] receivedBytes = null;
+            byte[] outData = null;
+
+            ctlCrcCalc1.LibConvertStringToBytesArr(rtbCmd.Text, ref outData);
+            rtbLog.Text += "<< " + BitConverter.ToString(outData) + "\n";
+
+            int res = vp.WriteReadData(FindPacketSignature, outData, ref receivedBytes, outData.Length, -1);
+            if (res > 0)
+            {
+                rtbLog.Text += ">> " + BitConverter.ToString(receivedBytes) + "\n";
+            } else
+            {
+                rtbLog.Text += ">> ответа нет \n";
+            }
+        }
+
+        public int FindPacketSignature(Queue<byte> queue)
+        {
+            return -1;
+        }
+    }
+}
